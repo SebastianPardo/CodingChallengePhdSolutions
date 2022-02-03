034c30740aa50cc1b7f298e447b5a0eb5d6fb14e
Juan Sebastian Pardo Jimenez <js.pardo.j@gmail.com>
2022-02-03 12:12:13 -0500

    Add viewcomponent historic orders

---
+++ BookStore/ViewComponents/HistoricOrdersViewComponent.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using BookStore.Data;
+
+namespace ViewComponentSample.ViewComponents
+{
+  public class HistoricOrdersViewComponent : ViewComponent
+  {
+    private readonly DataModel context;
+
+    public HistoricOrdersViewComponent(DataModel context)
+    {
+      this.context = context;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+      var items = await GetOrdersAsync();
+      return View(items);
+    }
+    private Task<List<BookByOrder>> GetOrdersAsync()
+    {
+      return context.BookByOrder.Join(context.Book,
+        bookByOrder => bookByOrder.IdBook,
+        book => book.Id,
+        (bookByOrder, book) => new BookByOrder
+        {
+          IdOrder = bookByOrder.IdOrder,
+          Order = null,
+          Quatity = bookByOrder.Quatity,
+          IsPreorder = bookByOrder.IsPreorder,
+          Book = book
+        }
+        ).Join(context.Order,
+        bookByOrder => bookByOrder.IdOrder,
+        order => order.Id,
+        (bookByOrder, order) => new BookByOrder
+        {
+          Quatity = bookByOrder.Quatity,
+          IsPreorder = bookByOrder.IsPreorder,
+          Book = bookByOrder.Book,
+          Order = order
+        }).Where(x => x.IsPreorder == false).ToListAsync();
+    }
+  }
+}
